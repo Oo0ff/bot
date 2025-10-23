f6b67f4b6a01bec9fc2e3ec8ff6ab69baa45da68
Oo0ff <kalenen02@mail.ru>
2025-10-23 14:54:06 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
+// Инициализация Telegram Web App
 let tg = window.Telegram.WebApp;
 tg.expand();
 
+// Настройка основной кнопки
 tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#2cab37";
+tg.MainButton.color = "#2c3e50";
 
 let selectedGenre = "";
 
-// Получаем все кнопки
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
+// Получение всех кнопок жанров
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const btn5 = document.getElementById("btn5");
+const btn6 = document.getElementById("btn6");
 
-// Обработчики для кнопок
+// Обработчики для кнопок жанров
 btn1.addEventListener("click", function() {
     toggleMainButton("comedy", "Показать комедии");
 });
@@ -39,9 +41,11 @@ btn6.addEventListener("click", function() {
     toggleMainButton("horror", "Показать ужасы");
 });
 
+// Функция переключения основной кнопки
 function toggleMainButton(genre, text) {
     if (tg.MainButton.isVisible) {
         tg.MainButton.hide();
+        selectedGenre = "";
     } else {
         tg.MainButton.setText(text);
         selectedGenre = genre;
@@ -51,10 +55,12 @@ function toggleMainButton(genre, text) {
 
 // Обработка нажатия основной кнопки
 Telegram.WebApp.onEvent("mainButtonClicked", function() {
-    tg.sendData(selectedGenre);
+    if (selectedGenre) {
+        tg.sendData(selectedGenre);
+    }
 });
 
-// Показываем информацию о пользователе (опционально)
-let usercard = document.getElementById("usercard");
+// Показ информации о пользователе
+let userinfo = document.getElementById("userinfo");
 let profileName = tg.initDataUnsafe.user?.first_name || "Пользователь";
-usercard.innerHTML = `<p>Добро пожаловать, ${profileName}!</p>`;
+userinfo.innerHTML = `<p>Добро пожаловать, ${profileName}!</p>`;
